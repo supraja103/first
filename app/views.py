e22c1a44753a76f6d44544f99543e2b8a54776d5
supraja103 <suprajadesula103@gmail.com>
2023-11-08 10:38:42 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def jampandu(request):
+    return HttpResponse('Hi Jampandu How r u')
+def jigelrani(request):
+    return HttpResponse('fyn')
